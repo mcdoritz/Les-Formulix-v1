3803b45d80be48e6e5a01d9fd190f58041a40921
mcdoritz <fdorizon@ikmail.com>
2024-04-11 13:51:13 +0200

    Mise en page dashboard : actualités

--- src/main/java/fr/lesformulix/controllers/app/App.java
+++ src/main/java/fr/lesformulix/controllers/app/App.java
@@ -14,6 +14,6 @@ public class App {
         prln("Controller app");
         model.addAttribute("pageTitle", "Les Formulix");
 
-        return "app/landing";
+        return "app/dashboard";
     }
 }
